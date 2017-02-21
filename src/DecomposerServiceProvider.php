77642bbc09c693c7b26ac9fedf79d40fa78c1f66
Etienne Marais <etienne.m@superbalist.com>
2017-02-21 09:43:57 +0200

    Fixed the PSR-2 formatting issues. Added some docblock comments.

--- src/DecomposerServiceProvider.php
+++ src/DecomposerServiceProvider.php
@@ -6,12 +6,13 @@ use Illuminate\Support\ServiceProvider;
 
 class DecomposerServiceProvider extends ServiceProvider
 {
-	public function boot()
-	{
-	    $this->loadViewsFrom(__DIR__.'/views', 'Decomposer');
-	}
+    public function boot()
+    {
+        $this->loadViewsFrom(__DIR__.'/views', 'Decomposer');
+    }
+    
     public function register()
     {
-        
+		// Don't register anything for now
     }
 }
--- src/controllers/DecomposerController.php
+++ src/controllers/DecomposerController.php
@@ -7,26 +7,30 @@ use Illuminate\Support\Collection;
 
 class DecomposerController extends Controller
 {
-	public function index()
-	{	
-		$json = file_get_contents(base_path('composer.json'));
-		$composerArray = json_decode($json,true);
-		$packagesArray = $composerArray['require'];
 
-		foreach($packagesArray as $key => $value)
-		{
-			if($key == 'php') {}
-			else
-			{
-				$json2 = file_get_contents(base_path("/vendor/{$key}/composer.json"));
-				$dependenciesArray = json_decode($json2,true);
-				$dependencies = $dependenciesArray['require'];				
-				$packages[] = ['name' => $key,
-				               'version' => $value,
-				               'dependencies' => $dependencies];
-			}				
-		}
+    /**
+     * @return Illuminate\View\View
+     */
+    public function index()
+    {
+        $json = file_get_contents(base_path('composer.json'));
+        $composerArray = json_decode($json, true);
+        $packagesArray = $composerArray['require'];
 
-		return view('Decomposer::index',compact('packages'));
-	}
+        foreach ($packagesArray as $key => $value) {
+            if ($key !== 'php') {
+                $json2 = file_get_contents(base_path("/vendor/{$key}/composer.json"));
+                $dependenciesArray = json_decode($json2, true);
+                $dependencies = $dependenciesArray['require'];
+                
+                $packages[] = [
+                    'name' => $key,
+                    'version' => $value,
+                    'dependencies' => $dependencies
+                ];
+            }
+        }
+
+        return view('Decomposer::index', compact('packages'));
+    }
 }
